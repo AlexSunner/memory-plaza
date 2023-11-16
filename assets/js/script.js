e7a5148954c36512dc05bc4f2abb31875490b72b
AlexSunner <>
2023-11-16 17:58:37 +0000

    Add fontawsome folder and functions to flipped cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     let flippedCards = [];
+    let moves = 0;
 
-    /*
-    * Checks if the card is already flipped
-    */
     function flipCard(card) {
-        if (card.classList.contains('flipped') || flippedCards.length === 2) {
+        if (flippedCards.length === 2) {
             return;
         }
+
         card.classList.add('flipped');
         flippedCards.push(card);
 
         if (flippedCards.length === 2) {
+            moves++;
             setTimeout(checkForMatch, 1000);
         }
     }
 
-    /*
-    * Checks if the cards match
-    */
     function checkForMatch() {
         let [card1, card2] = flippedCards;
-        if (card1.textContent === card2.textContent) {
+        let icon1 = card1.querySelector('.card-icon').classList[1];
+        let icon2 = card2.querySelector('.card-icon').classList[1];
+
+        if (icon1 === icon2) {
             console.log('Match!');
             alert(`That's right! You won!`);
-            throw (`That's right! You won!`);
         } else {
             card1.classList.remove('flipped');
             card2.classList.remove('flipped');
         }
 
         flippedCards = [];
+        updateMovesDisplay();
     }
 
-    /*
-    * Array of icons used as memory card symbols
-    * Duplicate the icons in order to create pairs for the game
-    * A shuffle function to randomize the array
-    */
     let icons = ['fa-heart', 'fa-star', 'fa-smile', 'fa-bolt', 'fa-flag', 'fa-moon'];
-
     let iconPairs = icons.concat(icons);
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
     shuffleArray(iconPairs);
 
     let cards = document.querySelectorAll('.card');
@@ -59,4 +45,15 @@ document.addEventListener('DOMContentLoaded', function () {
         card.innerHTML = `<i class="card-icon fas ${iconClass}"></i>`;
     });
 
-})
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    function updateMovesDisplay() {
+        console.log(`Moves: ${moves}`);
+    }
+
+});
